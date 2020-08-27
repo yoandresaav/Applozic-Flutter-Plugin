dabecf10338fe55e858af24a44b3aed711541f79
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2020-08-26 23:04:22 -0500

    add new 1

--- android/src/main/java/com/example/applozic_flutter/ApplozicFlutterPlugin.java
+++ android/src/main/java/com/example/applozic_flutter/ApplozicFlutterPlugin.java
@@ -223,7 +223,7 @@ public class ApplozicFlutterPlugin implements MethodCallHandler {
                 result.error(ERROR, "User not authorised. UserId is empty", null);
             }
         } else if (call.method.equals("isApplozicNotification")) {
-            if (Applozic.isApplozicNotification(this, call.arguments)){
+            if (Applozic.isApplozicNotification(context, call.arguments)){
                 result.success(0);
             } else {
                 result.success(1);
